0f3593d44587929b66ae3e3a502ffb85f8f1333c
testkkj <46573845+testkkj@users.noreply.github.com>
2020-01-27 01:24:10 +0900

    4장
    4장 예제 파일
    lombok 추가
    dto 파일 추가 및 lombok적용

---
+++ board/src/main/java/board/dto/BoardDto.java
@@ -0,0 +1,19 @@
+package board.dto;
+
+import lombok.Data;
+
+@Data
+/*
+ * lombok의 어노테이션으로 모든 필드의 getter/setter를 생성하고 tostring,hashcode/equals 메소드도 생성
+ * setter의 경우 final이 아닌 필드만 생성
+ */
+public class BoardDto {
+	private int boardIndx;
+	private String title;
+	private String contents;
+	private int hitCnt;
+	private String creatorId;
+	private String createdDatetime;
+	private String updaterId;
+	private String updateDatetime;
+}
