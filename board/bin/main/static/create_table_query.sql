b3813fafeee8a77d3435acc53a6d6af82539bda0
testkkj <46573845+testkkj@users.noreply.github.com>
2020-02-02 18:58:33 +0900

    4장
    4장 예제 파일
    sql-board.xml 패키지명 수정
    쿼리문 오타 수정
    boardList.html 오타 수정
    runwith 어노테이션용 junit4 추가(그레이들)

--- board/bin/main/static/create_table_query.sql
+++ board/bin/main/static/create_table_query.sql
@@ -2,7 +2,7 @@ CREATE TABLE t_board(
 board_idx INT(11) NOT NULL AUTO_INCREMENT COMMENT '글 번호',
 title VARCHAR(300) NOT NULL COMMENT '제목',
 contents TEXT NOT NULL COMMENT '내용',
-hint_cnt SMALLINT(10) NOT NULL DEFAULT '0' COMMENT '조회수',
+hit_cnt SMALLINT(10) NOT NULL DEFAULT '0' COMMENT '조회수',
 created_datetime DATETIME NOT NULL COMMENT '작성시간',
 creator_id VARCHAR(50) NOT NULL COMMENT '작성자',
 updated_datetime DATETIME DEFAULT NULL COMMENT '수정시간',
