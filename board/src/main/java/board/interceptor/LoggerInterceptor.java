acdc6088dc996655e895c471d925b5499a40769a
testkkj <46573845+testkkj@users.noreply.github.com>
2020-02-13 23:47:56 +0900

    5장
    5장 예제 파일
    인터셉터 출력 수정중
    aop 적용하기 시작

--- board/src/main/java/board/interceptor/LoggerInterceptor.java
+++ board/src/main/java/board/interceptor/LoggerInterceptor.java
@@ -8,19 +8,17 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 @Slf4j
-public class LoggerInterceptor extends HandlerInterceptorAdapter {
+public class LoggerInterceptor extends HandlerInterceptorAdapter{
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {
-        log.debug("========================= START =========================");
-        log.debug(" Request URI \t: " + request.getRequestURI());
-        return super.preHandle(request, response, handler);
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-            ModelAndView modelAndView) throws Exception {
-        log.debug("========================= END =========================");
-    }
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		log.debug("======================================          START         ======================================");
+		log.debug(" Request URI \t:  " + request.getRequestURI());
+		return super.preHandle(request, response, handler);
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		log.debug("======================================           END          ======================================\n");
+	}
 }
