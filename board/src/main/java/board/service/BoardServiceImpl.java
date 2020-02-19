6d74bdcb4816bca802eb984760da90a62ed08b88
testkkj <46573845+testkkj@users.noreply.github.com>
2020-02-19 22:16:13 +0900

    6장
    6장 예제 파일
    첨부파일 dto 및
    파일처리 클래스 작성

--- board/src/main/java/board/service/BoardServiceImpl.java
+++ board/src/main/java/board/service/BoardServiceImpl.java
@@ -1,14 +1,20 @@
 package board.service;
 
-import board.dto.BoardDto;
-import board.mapper.BoardMapper;
+import java.util.Iterator;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import java.util.List;
+import board.dto.BoardDto;
+import board.mapper.BoardMapper;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @Transactional
 public class BoardServiceImpl implements BoardService {
@@ -22,14 +28,30 @@ public class BoardServiceImpl implements BoardService {
 	}
 
 	@Override
-	public void insertBoard(BoardDto board) throws Exception {
+	public void insertBoard(BoardDto board, MultipartHttpServletRequest multipartHttpServletRequest) throws Exception {
 		boardMapper.insertBoard(board);
+		if (ObjectUtils.isEmpty(multipartHttpServletRequest) == false) {
+			Iterator<String> iterator = multipartHttpServletRequest.getFileNames();
+			String name;
+			while (iterator.hasNext()) {
+				name = iterator.next();
+				log.debug("file tag name: " + name);
+				List<MultipartFile> list = multipartHttpServletRequest.getFiles(name);
+				for (MultipartFile multipartFile : list) {
+					log.debug("start file information");
+					log.debug("file name : " + multipartFile.getOriginalFilename());
+					log.debug("file size : " + multipartFile.getSize());
+					log.debug("file content type : " + multipartFile.getContentType());
+					log.debug("end file information.\n");
+				}
+			}
+		}
 	}
 
 	@Override
 	public BoardDto selectBoardDetail(int boardIdx) throws Exception {
 		boardMapper.updateHitCount(boardIdx);
-		
+
 		BoardDto board = boardMapper.selectBoardDetail(boardIdx);
 
 		return board;
